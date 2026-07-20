bf0e1cd221eb0068c5ef5a1f4edb56c5aa24bbb9
mprathapmprathap18-prog <mprathapmprathap18@gmail.com>
2026-07-20 18:41:01 +0530

    Create studentProfile model with schema definition

---
+++ src/models/studentProfile.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const studentProfileSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  name: String,
+  dept: String,
+  year: String,
+  college: String,
+  hostel_name: String,
+  manager_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  tutor_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("StudentProfile", studentProfileSchema);
