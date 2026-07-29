5f8d53a640c8a2cc90e9e4de06ad96ef83d8e486
mprathapmprathap18-prog <mprathapmprathap18@gmail.com>
2026-07-29 19:04:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 // mongoo db
+console.log("MONGO_URI =", process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("✅ MongoDB Connected!");
